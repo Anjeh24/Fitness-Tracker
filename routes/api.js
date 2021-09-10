6f2571e3023a646a404a646ce3025e46dad3a130
Anjeh Mbah <junez24@yahoo.com>
2021-09-10 11:58:52 -0400

    api routes

---
+++ routes/api.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const fitness = require("../models/fitness.js");
+const Transaction = require("../models/fitness.js");
+
+router.post("/api/fitness", ({ body }, res) => {
+  fitness.create(body)
+    .then(dbfitness => {
+      res.json(dbfitness);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+
+
+router.get("/api/fitness", (req, res) => {
+  Transaction.find({})
+    .sort({ date: -1 })
+    .then(dbfitness => {
+      res.json(dbfitness);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
